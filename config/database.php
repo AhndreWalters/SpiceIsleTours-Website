d31cb41346b15a889ff0be85aac4364693e9c012
AhndreWalters <120767274+AhndreWalters@users.noreply.github.com>
2025-12-01 02:08:36 +0000

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,9 +1,9 @@
 <?php
-$host = getenv('DB_HOST') ?: 'localhost';
-$dbname = getenv('DB_NAME') ?: 'spice_isle_tours';
-$username = getenv('DB_USER') ?: 'avnadmin';
-$password = getenv('DB_PASSWORD') ?: '';
-$port = getenv('DB_PORT') ?: '24791';
+$host = 'mysql-13861cad-walterstrahndre-2a07.j.aivencloud.com';
+$dbname = 'spice_isle_tours';
+$username = 'avnadmin';
+$password = 'AVNS_YWYg5JNJWfmJyiPmOPo';
+$port = '24791';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname; port= $port", $username, $password);
