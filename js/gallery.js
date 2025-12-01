2a6ea48ad8ecaff478df6953c17d8cf0a4878ef0
AhndreWalters <120767274+AhndreWalters@users.noreply.github.com>
2025-12-01 00:25:15 +0000

    Update gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -4,21 +4,21 @@ const lightboxImg = document.getElementById('lightbox-img');
 const closeBtn = document.querySelector('.close');
 
 gallery.addEventListener('click', (e) => {
-  if (e.target.tagName.toLowerCase() === 'img') {
-    lightbox.style.display = 'block';
-    lightboxImg.src = e.target.src;
-    document.body.style.overflow = 'hidden';
-  }
+    if (e.target.tagName.toLowerCase() === 'img') {
+        lightbox.style.display = 'block';
+        lightboxImg.src = e.target.src;
+        document.body.style.overflow = 'hidden';
+    }
 });
 
 closeBtn.addEventListener('click', () => {
-  lightbox.style.display = 'none';
-  document.body.style.overflow = 'auto';
+    lightbox.style.display = 'none';
+    document.body.style.overflow = 'auto';
 });
 
 lightbox.addEventListener('click', (e) => {
-  if (e.target === lightbox) {
-    lightbox.style.display = 'none';
-    document.body.style.overflow = 'auto';
-  }
+    if (e.target === lightbox) {
+        lightbox.style.display = 'none';
+        document.body.style.overflow = 'auto';
+    }
 });
